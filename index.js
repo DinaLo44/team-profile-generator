63b1330c71e29778ceb9653a2fb6435d2b526555
DinaLo <dinalo.brady@gmail.com>
2022-08-22 18:27:23 -0600

    working on creating html

--- index.js
+++ index.js
@@ -10,6 +10,40 @@ const Intern = require('./lib/Intern');
 const teamMembers = [];
 
 const showMenu = () => {
+    const managerPrompt = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the manager?',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'What is its Employee ID?',
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: 'What is its email address?',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'What is its office number?',
+                name: 'officeNumber'
+            },
+        ]).then((response) => {
+            console.log(response);
+            const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
+            teamMembers.push(manager)
+            showMenu();
+        }).catch((error) => {
+            console.error(error)
+            throw error
+        })
+    };
+    
+    managerPrompt();
     inquirer.prompt([
         {
             type: 'list',
@@ -26,117 +60,90 @@ const showMenu = () => {
                 internPrompt();
                 break;
             default:
-                console.log('I have finished to add the members of my team')
+                generateHtml();
 
         }
     })
-}
+    
+    function generateHtml() {
+        let htmlFile = generateMarkdown(teamMembers);
+            fs.writeFile('./dist/myteam.html', htmlFile, "utf-8");
+    }
+    const engineerPrompt = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is the name of the engineer?',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'What is its Employee ID?',
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: 'What is its email address?',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'What is its GitHub username?',
+                name: 'github'
+            },
+        ]).then((response) => {
+            console.log(response);
+            const engineer = new Engineer(response.name, response.id, response.email, response.officeNumber);
+            teamMembers.push(engineer)
+            showMenu();
+        }).catch((error) => {
+            console.error(error)
+            throw error
+        })
+    };
 
-const managerPrompt = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the name of the manager?',
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: 'What is its Employee ID?',
-            name: 'id',
-        },
-        {
-            type: 'input',
-            message: 'What is its email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is its office number?',
-            name: 'officeNumber'
-        },
-    ]).then((response) => {
-        console.log(response);
-        const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
-        teamMembers.push(manager)
-        showMenu();
-    }).catch((error) => {
-        console.error(error)
-        throw error
-    })
-};
+    const internPrompt = () => {
+        inquirer.prompt([
+            {
+                type: 'input',
+                message: 'What is name of the intern?',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'What is its Employee ID?',
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: 'What is its email address?',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'What is the name of the school he/she is currently attending?',
+                name: 'school'
+            },
+        ]).then((response) => {
+            console.log(response);
+            const intern = new Intern(response.name, response.id, response.email, response.officeNumber);
+            teamMembers.push(intern)
+            showMenu();
+        }).catch((error) => {
+            console.error(error)
+            throw error
+        })
+        
+    };
 
-const engineerPrompt = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the name of the engineer?',
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: 'What is its Employee ID?',
-            name: 'id',
-        },
-        {
-            type: 'input',
-            message: 'What is its email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is its GitHub username?',
-            name: 'github'
-        },
-    ]).then((response) => {
-        console.log(response);
-        const engineer = new Engineer(response.name, response.id, response.email, response.officeNumber);
-        teamMembers.push(engineer)
-        showMenu();
-    }).catch((error) => {
-        console.error(error)
-        throw error
-    })
-};
-
-const internPrompt = () => {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is name of the intern?',
-            name: 'name'
-        },
-        {
-            type: 'input',
-            message: 'What is its Employee ID?',
-            name: 'id',
-        },
-        {
-            type: 'input',
-            message: 'What is its email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'What is the name of the school he/she is currently attending?',
-            name: 'school'
-        },
-    ]).then((response) => {
-        console.log(response);
-        const intern = new Intern(response.name, response.id, response.email, response.officeNumber);
-        teamMembers.push(intern)
-        showMenu();
-    }).catch((error) => {
-        console.error(error)
-        throw error
-    // }).then(function () {
-    //     let htmlFile = generateMarkdown(teamMembers);
-    //     fs.writeFile('./dist/myteam.html', htmlFile, "utf-8");
-    });
 }
 
 
-managerPrompt()
 
 
 
+// .then(function () {
+//     
 
+// managerPrompt()
+showMenu()
